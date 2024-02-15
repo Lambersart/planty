67581004222faba037629c269a8aba8f35862a7f
Lambersart <skijek@gmail.com>
2024-02-15 17:15:33 +0100

    u15fevrier

--- wp-content/themes/PLANTY/functions.php
+++ wp-content/themes/PLANTY/functions.php
@@ -7,3 +7,12 @@ function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+
+add_filter( 'wp_nav_menu_items', 'ajoutadmin', 10, 2 );
+function ajoutadmin( $items, $args ) {
+    if (is_user_logged_in() ) {
+        $items .= '<li><a href="/wp-admin">Admin</a></li>';
+    }
+    return $items;
+}
